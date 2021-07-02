4f6034603da38510026e669280e821b997416fbf
Ubuntu <ubuntu@ip-172-31-67-65.ec2.internal>
2021-07-02 21:52:59 +0000

    added new outputs and map transformations

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@
 #   value       = flatten(module.container[*].ip-address)
 #   description = "The IP address and external port of the container"
 # }
+
+output "application_access" {
+    value = [for x in module.container[*]: x]
+    description = "The name and socket for each application."
+}

1bfe7571c6f32afa90ebdbce2cd6461eeb75a5fc
Ubuntu <ubuntu@ip-172-31-67-65.ec2.internal>
2021-07-02 18:07:12 +0000

    added docker volume, lifecycle targeting, and multi use resource

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-resource "null_resource" "dockervol" {
-  provisioner "local-exec" {
-    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
-  }
+
+module "nodered_image" {
+  source   = "./image"
+  image_in = var.image["nodered"][terraform.workspace]
 }
 
-module "image" {
+module "influxdb_image" {
   source   = "./image"
-  image_in = var.image[terraform.workspace]
+  image_in = var.image["influxdb"][terraform.workspace]
 }
+
 
 resource "random_string" "random" {
   count   = local.container_count
@@ -19,13 +20,12 @@ resource "random_string" "random" {
 
 module "container" {
   source            = "./container"
-  depends_on        = [null_resource.dockervol]
   count             = local.container_count
   name_in           = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image_in          = module.image.image_out
+  image_in          = module.nodered_image.image_out
   int_port_in       = var.int_port
   ext_port_in       = var.ext_port[terraform.workspace][count.index]
   container_path_in = "/data"
-  host_path_in      = "${path.cwd}/noderedvol"
 }
+
 
